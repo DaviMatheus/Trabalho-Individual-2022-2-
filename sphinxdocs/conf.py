a1fa3d5c6f9acd4761f4327ad12363126f07991f
DaviMatheus <matadavimat@gmail.com>
2023-01-29 20:25:00 -0300

    Item4_FIX

--- sphinxdocs/conf.py
+++ sphinxdocs/conf.py
@@ -39,7 +39,7 @@ extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "b
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-breathe_projects = { "sphinxdocs": "../doc/xml/index.xml"}
+breathe_projects = { "sphinxdocs": "../doxygen/xml/"}
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
