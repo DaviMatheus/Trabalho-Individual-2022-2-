6cac72ec6bd249ffafaa14bf7d40902254c49ec5
DaviMatheus <matadavimat@gmail.com>
2023-01-29 20:21:02 -0300

    Item4_Integrando_breath

--- sphinxdocs/conf.py
+++ sphinxdocs/conf.py
@@ -34,12 +34,12 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "breathe"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+breathe_projects = { "sphinxdocs": "../doc/xml/index.xml"}
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
